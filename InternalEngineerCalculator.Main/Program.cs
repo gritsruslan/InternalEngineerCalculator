c6dea2f2bc1b9b2fafdbda1754fbe1c548d7b116
gritsruslan <gritsruslan@gmail.com>
2024-12-03 23:45:18 +0300

    Add basic lexer and some fixes

--- InternalEngineerCalculator.Main/Program.cs
+++ InternalEngineerCalculator.Main/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Mime;
+﻿using System.Globalization;
+using InternalEngineerCalculator.Main.Common;
 
 namespace InternalEngineerCalculator.Main;
 
@@ -6,7 +7,19 @@ static class Program
 {
 	static void Main()
 	{
-		Console.WriteLine("Hello, world!");
+		while (true)
+		{
+			Console.Write("> ");
+			var code = Console.ReadLine();
+			if (code is null)
+				continue;
+
+			var tokens = new Lexer(code).Tokenize();
+
+			foreach (var token in tokens)
+				Console.WriteLine(token.ToString());
+
+		}
 	}
 }
 
@@ -18,9 +31,11 @@ public enum TokenType
 	Minus,
 	Divide,
 	Multiply,
-	LeftParenthesis,
-	RightParenthesis,
-	Dot
+	OpenParenthesis,
+	CloseParenthesis,
+	Dot,
+	EndOfLine,
+	Unknown
 }
 
 internal abstract class Token
@@ -38,12 +53,12 @@ internal sealed class NumberToken(string text, float value) : Token
 
 	public override TokenType Type { get; set; } = TokenType.Number;
 
-	public float Value { get; set; }
+	public float Value { get; set; } = value;
 
 	public override string ToString() => $"{nameof(NumberToken)} : {Value}";
 }
 
-internal sealed class OperationToken (string text, TokenType type) : Token
+internal sealed class NonValueToken (string text, TokenType type) : Token
 {
 
 	public override string Text { get; set; } = text;
@@ -51,4 +66,109 @@ internal sealed class OperationToken (string text, TokenType type) : Token
 	public override TokenType Type { get; set; } = type;
 
 	public override string ToString() => Type.ToString();
+}
+
+internal sealed class Lexer(string code)
+{
+	private List<Token> _tokens = [];
+
+	private string _code = code;
+
+	private int _index = 0;
+
+	private char Current => _index < _code.Length ? _code[_index] : '\0';
+
+	private void Next() => _index++;
+
+	private void SkipWhitespaces()
+	{
+		while(Current != '\0' && char.IsWhiteSpace(Current))
+			Next();
+	}
+
+	private readonly HashSet<char> _singleChars = ['+', '-', '*', '/', '(', ')'];
+
+	private bool IsSingleChar(char chr) => _singleChars.Contains(chr);
+
+	public List<Token> Tokenize()
+	{
+		while (true)
+		{
+			var token = NextToken();
+
+			if(token.Type == TokenType.EndOfLine)
+				break;
+
+			_tokens.Add(token);
+		}
+
+		return _tokens;
+	}
+
+	private Token NextToken()
+	{
+		SkipWhitespaces();
+
+		if (Current == '\0')
+			return new NonValueToken("\0", TokenType.EndOfLine);
+
+		var numberTokenOpt = ProcessIfNumberToken();
+		if (numberTokenOpt.IsSome)
+			return numberTokenOpt.Unwrap();
+
+		var singleCharToken = ProcessIfSingleCharToken();
+		if (singleCharToken.IsSome)
+			return singleCharToken.Unwrap();
+
+		throw new Exception("Unknown Token");
+	}
+
+	private Option<NumberToken> ProcessIfNumberToken()
+	{
+		if (!char.IsDigit(Current))
+			return Option<NumberToken>.None;
+
+		const char dot = '.';
+		var tokenString = string.Empty;
+		bool hasDot = false;
+
+		do
+		{
+			if (Current == dot && !hasDot)
+				hasDot = true;
+			else if (Current == dot && hasDot)
+				throw new Exception();
+
+			tokenString += Current;
+
+			 Next();
+		}
+		while (char.IsDigit(Current) || Current == dot);
+
+		if (!float.TryParse(tokenString, NumberStyles.Float, CultureInfo.InvariantCulture, out var tokenValue))
+			throw new Exception();
+
+		return new NumberToken(tokenString, tokenValue);
+	}
+
+	private Option<NonValueToken> ProcessIfSingleCharToken()
+	{
+		if (!IsSingleChar(Current))
+			return Option<NonValueToken>.None;
+
+		NonValueToken singleCharToken = Current switch
+		{
+			'+' => new NonValueToken("+", TokenType.Plus),
+			'-' => new NonValueToken("-", TokenType.Minus),
+			'*' => new NonValueToken("-", TokenType.Multiply),
+			'/' => new NonValueToken("-", TokenType.Divide),
+			'(' => new NonValueToken("(", TokenType.OpenParenthesis),
+			')' => new NonValueToken(")", TokenType.CloseParenthesis),
+			_ => throw new Exception()
+		};
+
+		Next();
+
+		return singleCharToken;
+	}
 }
