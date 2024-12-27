4b87e9157c8405f72f51f1817d70c981bc6f184f
gritsruslan <gritsruslan@gmail.com>
2024-12-27 17:16:44 +0400

    change inheritance for variable assignment expression

--- InternalEngineerCalculator.Main/Expressions/VariableAssignmentExpression.cs
+++ InternalEngineerCalculator.Main/Expressions/VariableAssignmentExpression.cs
@@ -2,13 +2,13 @@ using InternalEngineerCalculator.Main.Tokens;
 
 namespace InternalEngineerCalculator.Main.Expressions;
 
-internal class VariableAssignmentExpression(Token identifierToken, Expression variableValueExpression) : Expression
+internal class VariableAssignmentExpression(Token identifierToken, Expression variableValueExpression) : AssignmentExpression
 {
 	public Token IdentifierToken { get; } = identifierToken;
 
 	public Expression VariableValueExpression { get; } = variableValueExpression;
 
-	public string VariableName => identifierToken.ValueString;
+	public override string Name => IdentifierToken.ValueString;
 
 	public override TokenType Type => TokenType.VariableAssignmentExpression;
 }
