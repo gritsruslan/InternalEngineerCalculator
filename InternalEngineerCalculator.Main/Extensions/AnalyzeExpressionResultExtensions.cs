404226c1014a0b76aec745da92dfbecff326a7f6
gritsruslan <gritsruslan@gmail.com>
2025-01-08 20:01:37 +0400

    replace stringify to print

--- InternalEngineerCalculator.Main/Extensions/AnalyzeExpressionResultExtensions.cs
+++ InternalEngineerCalculator.Main/Extensions/AnalyzeExpressionResultExtensions.cs
@@ -5,35 +5,33 @@ namespace InternalEngineerCalculator.Main.Extensions;
 
 internal static class AnalyzeExpressionResultExtensions
 {
-	public static string Print(this AnalyzeExpressionResult result)
+	public static void PrettyPrint(this AnalyzeExpressionResult result)
 	{
-		var sb = new StringBuilder();
-
 		//undefined functions
 		foreach (var undefinedFunctionStackCall in result.UndefinedFunctions)
 		{
 			var undefinedFunction = undefinedFunctionStackCall.Last();
-			sb.Append($"Undefined function {undefinedFunction.FunctionName} with {undefinedFunction.CountOfArg} in ");
+			Console.Write($"Undefined function {undefinedFunction.FunctionName} with {undefinedFunction.CountOfArg} in ");
 
 			for (int i = 0; i < undefinedFunctionStackCall.Length; i++)
 			{
 				var function = undefinedFunctionStackCall[i];
 				if (i == undefinedFunctionStackCall.Length - 1)
 				{
-					sb.Append(function.ToPrettyString());
+					Console.Write(function.ToPrettyString());
 					continue;
 				}
-				sb.Append(function.ToPrettyString() + "->");
+				Console.Write(function.ToPrettyString() + "->");
 			}
 
-			sb.AppendLine();
+			Console.WriteLine();
 		}
 
 		//undefined variables
 		foreach (var undefinedVariableStackCall in result.UndefinedVariables)
 		{
 			var undefinedVariable = undefinedVariableStackCall.Item2;
-			sb.Append($"Undefined variable {undefinedVariable} in ");
+			Console.Write($"Undefined variable {undefinedVariable} in ");
 
 			var undefinedVariableFunctionStackCall = undefinedVariableStackCall.Item1;
 
@@ -42,16 +40,14 @@ internal static class AnalyzeExpressionResultExtensions
 				var function = undefinedVariableFunctionStackCall[i];
 				if (i == undefinedVariableFunctionStackCall.Length - 1)
 				{
-					sb.Append(function.ToPrettyString());
+					Console.Write(function.ToPrettyString());
 					continue;
 				}
-				sb.Append(function.ToPrettyString() + "->");
+				Console.Write(function.ToPrettyString() + " -> ");
 			}
 
-			sb.Append(undefinedVariable + "!");
-			sb.AppendLine();
+			Console.Write(undefinedVariable + "!");
+			Console.WriteLine();
 		}
-
-		return sb.ToString();
 	}
 }
